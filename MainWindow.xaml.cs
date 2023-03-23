b622b484ee162e73e617ad356fddee7685fa726b
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-03-22 23:12:56 -0700

    Added code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace Lecture_Example___Rich_Text_Box___Advanced__
     /// </summary>
     public partial class MainWindow : Window
     {
+       FlowDocument fdDisplay = new FlowDocument();
         public MainWindow()
-        {
+        {          
             InitializeComponent();
+           
+        }
+
+        public void Itworks()
+        {
+            Paragraph para = new Paragraph();
+            Paragraph para2 = new Paragraph();
+            Run run = new Run("Lecture Example Rich Text Box");
+            run.Background = new SolidColorBrush(Colors.DeepPink);
+            Run run1 = new Run("Hello Everyone");
+            para.Inlines.Add(run);
+            para.Background = new SolidColorBrush(Colors.Azure);
+            para2.Inlines.Add(run1);
+            fdDisplay.Blocks.Add(para);
+            fdDisplay.Blocks.Add(para2);
+            fdDisplay.Background = new SolidColorBrush(Colors.Bisque);
+            rtbDisplay.Document = fdDisplay;
+            rtbDisplay.Background = new SolidColorBrush(Color.FromRgb(25, 25, 25));
+        }
+        
+        public void FlowExample()
+        {
+            FlowDocument fdDisplay = new FlowDocument();
+            Paragraph p = new Paragraph();
+            Run r = new Run("Hi all");
+            r.FontWeight = FontWeights.Bold;
+            r.Foreground = new SolidColorBrush(Colors.Cyan);
+            r.FontSize = 18;
+            p.Inlines.Add(r);
+            fdDisplay.Blocks.Add(p);
+            rtbDisplay.Document = fdDisplay;
+        }
+
+        private void btnDisply_Click(object sender, RoutedEventArgs e)
+        {
+            Paragraph newParagraph = new Paragraph();
+            string subjectline = txtHeader.Text;
+            string body = rtbRunBody.Text;
+            Run headerrun = new Run(subjectline);
+            headerrun.FontSize = 18;
+            headerrun.Foreground = new SolidColorBrush(Colors.Cyan);
+            Run runNewBody = new Run(body);
+            runNewBody.FontSize = 16;
+            runNewBody.FontWeight = FontWeights.Bold;
+            runNewBody.Foreground = new SolidColorBrush(Colors.White);
+            newParagraph.Inlines.Add(headerrun);
+            newParagraph.Inlines.Add("\n");
+            newParagraph.Inlines.Add(runNewBody);
+            fdDisplay.Blocks.Add(newParagraph);
+            rtbDisplay.Document = fdDisplay;
         }
     }
 }
